ce033ac6f6bfe7e8a9411081d31d802abb99b127
AURO RUBENS SILVA NETO <auro.rsneto@senacsp.edu.br>
2025-04-08 14:26:16 -0300

    Ajeitando para deploy

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/home";
 import NotFound from "../pages/notFound/NotFound";
 import Treino from "../pages/treino";
 import Exercicio from "../pages/exercicio";
+import Login from "../pages/login/login";
 
 export default function AppRoutes() {
   return (
@@ -13,6 +14,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/treino" element={<Treino />} />
         <Route path="/exercicio" element={<Exercicio />} />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
